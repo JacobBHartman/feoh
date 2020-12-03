21d58721689613fc66977a3d97d07a7bdb32380e
JacobBHartman <jacobbhartman@protonmail.com>
2020-12-03 09:46:09 -0800

    put environment variable in script

--- ansible/jenkins/install-plugins.sh
+++ ansible/jenkins/install-plugins.sh
@@ -1,5 +1,6 @@
 filename=/tmp/plugins.txt
 lines=$(cat $filename)
+ADMIN_PASSWORD=$(cat /var/lib/jenkins/secrets/initialAdminPassword)
 for line in $lines;
 do
   java -jar /usr/local/bin/jenkins-cli.jar -s http://localhost:8080/ -auth admin:$ADMIN_PASSWORD install-plugin "$line"
