f2f4cea652204ecf834b1f83f379159dd00c77e2
JacobBHartman <jacobbhartman@protonmail.com>
2020-12-03 13:22:27 -0800

    add functionality to direct traffic to blog

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -24,3 +24,14 @@ resource "aws_route53_record" "deploy" {
     evaluate_target_health = true
   }
 }
+
+resource "aws_route53_record" "blog" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = "blog.bovine.biz"
+  type    = "A"
+  alias {
+    name    = aws_elb.web.dns_name
+    zone_id = aws_elb.web.zone_id
+    evaluate_target_health = true
+  }
+}
