31259f7c6ccc46ae288d07f2b6dd313ad28fa7a2
JacobBHartman <jacobbhartman@protonmail.com>
2021-01-11 10:41:58 -0800

    add hostname for masters ruby app

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -57,3 +57,14 @@ resource "aws_route53_record" "blog" {
     evaluate_target_health = true
   }
 }
+
+resource "aws_route53_record" "masters" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = "masters.bovine.biz"
+  type    = "A"
+  alias {
+    name    = aws_elb.web.dns_name
+    zone_id = aws_elb.web.zone_id
+    evaluate_target_health = true
+  }
+}
